3532cf1afbd3fe0634e9b54777f502c629ac5a1f
Abdelrahman mostafa <62848968+abdom0stafa@users.noreply.github.com>
2021-12-15 20:47:26 +0200

    i added a handel function for all the routes that are not cached by our toures

--- app.js
+++ app.js
@@ -11,18 +11,37 @@ if (process.env.NODE_ENV === 'development') {
   app.use(morgan('dev'));
 }
 
-app.use(express.json());
+app.use(express.json());//json parser
 app.use(express.static(`${__dirname}/public`));
 
+//* basic middelware
+// app.use((req, res, next) => {
+//   console.log('Hello from the middleware 👋');
+//   next();
+// });
+
 app.use((req, res, next) => {
-  console.log('Hello from the middleware 👋');
+  req.requestTime = new Date().toISOString();
   next();
 });
+
+
 
 
 // 3) ROUTES
 app.use('/api/v1/tours', tourRouter);
 app.use('/api/v1/users', userRouter);
+
+//^ what we want to implement is a route handler for a route that was not cached by any of  our route handlers
+//* since the code is executed are executed in order so if we have a request that makes it into this point here of our code this means that non of the above where able to catch it.
+
+//app.all it will run for all the http methods get patch post delete etc. * in the url stand for every thing
+app.all('*', (req, res) => {
+  res.status(404).json({
+    status: 'failed',
+    message:`this route ${req.originalUrl} is either not defined or you cant access it `
+  });
+});
 
 module.exports = app;
  
