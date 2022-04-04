0d850c02508f74925423277ea27f536d3f86fa44
Abdelrahman mostafa <62848968+abdom0stafa@users.noreply.github.com>
2022-04-04 23:46:09 +0200

    creating sending email to reset password functionality

---
+++ utils/email.js
@@ -0,0 +1,11 @@
+//! email handler function to be used accros the application
+const nodemailer = require('nodemailer');
+//! to be able to send email we have 3 steps:
+
+    //? 1)Create the transporter
+
+    //? 2)Define the email options
+
+
+
+
